81d7a9769d5a6081f44e14156ff0e1debc0e22e2
Anatoly03 <anatoly.wein@gmail.com>
2021-12-04 15:12:10 +0100

    add: player, game screen

--- src/js/game.ts
+++ src/js/game.ts
@@ -16,33 +16,43 @@ export const game = {
 		Pos.x[this.pid] = .5
 		Pos.y[this.pid] = .5
 
-		Size.r[this.pid] = .01
+		Size.r[this.pid] = .005
 	},
 
 	update() {
 		if ((keys['w'] && !keys['s'])) {
-			Pos.y[this.pid] -= .004
+			Pos.y[this.pid] -= .005
 		} else if (keys['s'] && !keys['w']) {
-			Pos.y[this.pid] += .004
+			Pos.y[this.pid] += .005
 			//} else {
 			//Control.y[this.pid] = 0
 		}
 
 		if (keys['d'] && !keys['a']) {
-			Pos.x[this.pid] += .004
+			Pos.x[this.pid] += .005
 		} else if (keys['a'] && !keys['d']) {
-			Pos.x[this.pid] -= .004
+			Pos.x[this.pid] -= .005
 			//} else {
 			//Control.x[this.pid] = 0
 		}
 	},
 
 	render() {
+		let offset = 50
+		let size = Math.min(width, height) - 2 * offset
+
 		ctx.fillStyle = 'black'
 		ctx.fillRect(0, 0, width, height)
 
+		// Gamescreen Border
+		ctx.strokeStyle = 'white'
+		ctx.lineWidth = 2
+		ctx.strokeRect(offset, offset, size, size)
+
 		ctx.fillStyle = 'green'
-		ctx.fillRect(Pos.x[this.pid] * width, Pos.y[this.pid] * height, Size.r[this.pid] * width, Size.r[this.pid] * height)
+		ctx.beginPath();
+		ctx.arc(offset + Pos.x[this.pid] * size, offset + Pos.y[this.pid] * size, Size.r[this.pid] * size, 0, 2 * Math.PI);
+		ctx.fill();
 	},
 
 	onKeyDown(event: KeyboardEvent) {
