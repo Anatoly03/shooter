5f36a35fad1386bf474434899c5881d81a65b5af
Anatoly03 <anatoly.wein@gmail.com>
2021-12-04 19:39:23 +0100

    add: test entities change: slow down: x

--- src/js/game.ts
+++ src/js/game.ts
@@ -1,10 +1,12 @@
 
 import { createWorld, addEntity, addComponent, pipe, defineQuery, removeEntity } from 'bitecs'
 import { ctx, width, height, keys, DEBUG_MODE, FPS } from "./app"
-import { Player, Pos, Size, Vel } from './comps'
+import { Acc, Bullet, Player, Pos, Size, Vel } from './comps'
 import physics from './physics'
 
 export const world = createWorld()
+
+const bullets = defineQuery([Bullet, Pos, Size])
 
 export const game = {
 	init() {
@@ -18,14 +20,37 @@ export const game = {
 		Pos.y[this.pid] = .5
 
 		Size.r[this.pid] = .005
+
+		this.spawn()
+	},
+
+	spawn() {
+		for (let i = 0; i < Math.random() * 25; i++) {
+			let eid = addEntity(world)
+			addComponent(world, Bullet, eid)
+			addComponent(world, Pos, eid)
+			addComponent(world, Size, eid)
+			addComponent(world, Vel, eid)
+			addComponent(world, Acc, eid)
+
+			Pos.x[eid] = Math.random()
+			Pos.y[eid] = 0
+
+			Acc.x[eid] = (Math.random() - .5) * .0001
+			Acc.y[eid] = Math.random() * .00005
+
+			Size.r[eid] = .005
+		}
+
+		setTimeout(this.spawn.bind(this), 200)
 	},
 
 	update() {
 		let x = (keys['a'] || keys['left'] ? -1 : 0) + (keys['d'] || keys['right'] ? 1 : 0)
 		let y = (keys['w'] || keys['up'] ? -1 : 0) + (keys['s'] || keys['down'] ? 1 : 0)
 
-		Vel.x[this.pid] = (keys[' '] ? .002 : .005) * x
-		Vel.y[this.pid] = (keys[' '] ? .002 : .005) * y
+		Vel.x[this.pid] = (keys['x'] ? .002 : .005) * x
+		Vel.y[this.pid] = (keys['x'] ? .002 : .005) * y
 
 		physics(world)
 	},
@@ -45,11 +70,20 @@ export const game = {
 		ctx.arc(offset + Pos.x[this.pid] * size, offset + Pos.y[this.pid] * size, Size.r[this.pid] * size, 0, 2 * Math.PI)
 		ctx.fill()
 
+		ctx.fillStyle = 'red'
+		const bullet_array = bullets(world)
+		for (let i = 0; i < bullet_array.length; i++) {
+			let eid = bullet_array[i]
+			ctx.beginPath()
+			ctx.arc(offset + Pos.x[eid] * size, offset + Pos.y[eid] * size, Size.r[eid] * size, 0, 2 * Math.PI)
+			ctx.fill()
+		}
+
 		ctx.textAlign = 'right'
 		ctx.textBaseline = 'bottom'
 		ctx.fillStyle = 'white'
 		ctx.font = '20px Righteous'
-		ctx.fillText(""+Math.round(FPS), size + offset - 5, size + offset - 5)
+		ctx.fillText("" + Math.round(FPS), size + offset - 5, size + offset - 5)
 
 		//
 		// UI
